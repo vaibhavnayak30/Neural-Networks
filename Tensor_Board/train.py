93f049efe4e54c47a1b0afd663210cd4c08e81a2
“Vaibhav” <vaibhavnayak30@gmail.com>
2022-12-11 14:52:36 +0530

    Changes added

--- Tensor_Board/train.py
+++ Tensor_Board/train.py
@@ -17,17 +17,24 @@ model = torch.nn.Linear(1,1)
 criterian = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
+# Training loop
 def train_model(iter):
     for epoch in range(iter):
         y1 = model(x)
         loss = criterian(y1, y)
+        # Add training scalers to tensorborad writer
         writer.add_scalar(tag='Loss/train', scalar_value=loss, global_step=epoch)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+# Start traininig loop
 train_model(10)
+
+# call flush() to write all pending events to disk
 writer.flush()
+
+# Close writer 
 writer.close()
 
 
